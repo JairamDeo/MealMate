c18fe3ddf0d3dd24c36e4062788050dcdea2ad50
Jairam Deo <jairamdeo2002@gmail.com>
2024-12-13 01:44:22 +0530

    url update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin","https://meal-mate-lime.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
